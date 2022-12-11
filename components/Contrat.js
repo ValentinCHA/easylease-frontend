633b7429b1fac124924faa31d3cd2fed3e7e2255
ezeflt <feltenezechiel1@gmail.com>
2022-12-11 16:08:27 +0100

    ajout de la page contrat

---
+++ components/Contrat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '../styles/Contrat.module.css'
+
+function Contrat() {
+  return (
+    <div>
+      <h1>Welcome contrat</h1>
+    </div>
+  )
+}
+
+export default Contrat
