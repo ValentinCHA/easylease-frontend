6b980be1de1e3f11e4c63fa847f89a47084f3899
ezeflt <108800520+ezeflt@users.noreply.github.com>
2022-12-11 23:41:50 +0100

    ajout de commentaire de la navBar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,37 +1,46 @@
 import React, { useDebugValue } from 'react'
 import style from '../styles/Navbar.module.css'
+
+//import des icon de la navbar
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserTie } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { faRectangleList } from '@fortawesome/free-solid-svg-icons'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faChartSimple } from '@fortawesome/free-solid-svg-icons'
 import { faDiagramNext } from '@fortawesome/free-solid-svg-icons'
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
-
-
+//import {use router} qui permet de changer de router 
 import {useRouter} from 'next/router'
+//import dispatch qui permet de d'activer les fonctions de mon reducer
 import { useDispatch } from 'react-redux'
+// fonction de mon fichier reducer/user
 import {logout} from '../reducers/user'
 
 function Navbar() {
 
+    // declaration de useRouter dans une const router
     const router = useRouter();
+    // declaration du dispatch dans une const dispatch
     const dispatch = useDispatch()
 
+    // fonction qui active {logout} et qui me renvoie vers ma page login
     const handleLogout =()=>{
         dispatch(logout())
         router.push('/login')
     }
 
-//bienvenue
+
   return (
+    // toute ma navbar 
     <div className={style.container}>
-      <div className={style.navbar}>
+        {/* taille de ma 1er navBar */}
+      <div className={style.firstNavbar}>
+        {/* logo navbar */}
         <img src='logo.png' className={style.logo}/>
+        {/* boite avec les infos du users */}
             <div className={style.accountBox} >
                 <FontAwesomeIcon icon={faUserTie} className={style.avatar} />
                     <div className={style.infoUser}>
@@ -39,27 +48,30 @@ function Navbar() {
                         <span className={style.emailSize}>JhonDoe@gmail.com</span>
                     </div>
             </div>
-            <div className={style.nav}>
-                <div className={style.navigation}  >
-                    <span><FontAwesomeIcon icon={faChartSimple} className={style.spaceIcon} />Dashboard</span>
+            {/* toutes mes icons qui vont permettre de naviguer de page en page */}
+            <div className={style.navigationPage}>
+                {/* espace entre chaque box */}
+                <div className={style.spaceBoxSpan}>
+                    <span><FontAwesomeIcon icon={faChartSimple} className={style.spaceIconSpan} />Dashboard</span> {/* espace entre l'icon et le text */}
                 </div>
-                <div className={style.navigation}>
-                    <span><FontAwesomeIcon icon={faDiagramNext} className={style.spaceIcon} />Scénario</span>
+                <div className={style.spaceBoxSpan}>
+                    <span><FontAwesomeIcon icon={faDiagramNext} className={style.spaceIconSpan} />Scénario</span>
                 </div>
-                <div className={style.navigation}>
-                    <span><FontAwesomeIcon icon={faEnvelope} className={style.spaceIcon} />Client</span>
+                <div className={style.spaceBoxSpan}>
+                    <span><FontAwesomeIcon icon={faEnvelope} className={style.spaceIconSpan} />Client</span>
                 </div>
-                <div className={style.navigation}>
-                    <span><FontAwesomeIcon icon={faUser} className={style.spaceIcon} />Contacts</span>
+                <div className={style.spaceBoxSpan}>
+                    <span><FontAwesomeIcon icon={faUser} className={style.spaceIconSpan} />Contacts</span>
                 </div>
             </div>
+            {/* taille de ma 2e navBar */}
             <div className={style.secondNav}>
-                <div className={style.navigation}>
-                    <span><FontAwesomeIcon icon={faEllipsis} className={style.spaceIcon} />Settings</span>
+                <div className={style.spaceBoxSpan}>
+                    <span><FontAwesomeIcon icon={faEllipsis} className={style.spaceIconSpan} />Settings</span>
                 </div>
                 <div className={style.footerNav}>
-                    <span onClick={()=>handleLogout()} className={style.fonsize} ><FontAwesomeIcon icon={faXmark} className={style.spaceIcon} />Logout</span>
-                    <span className={style.fonsize}><FontAwesomeIcon icon={faArrowLeft} className={style.spaceIconLogout} />Toogle SideBar</span>
+                    <span onClick={()=>handleLogout()} className={style.boxSpan} ><FontAwesomeIcon icon={faXmark} className={style.spaceIconSpan} />Logout</span>
+                    <span className={style.boxSpan}><FontAwesomeIcon icon={faArrowLeft} className={style.spaceIconLogout} />Toogle SideBar</span>
                 </div>
             </div>
       </div>
