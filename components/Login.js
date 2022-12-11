30bb4fdc9d3030050758d551015bccd4fc5dd17a
Valentin Charpentier <valentincharpentier@hotmail.com>
2022-12-11 15:47:55 +0100

    visuel terminé

--- components/Login.js
+++ components/Login.js
@@ -1,27 +1,55 @@
 import React, {useState} from "react";
-import Image from "next/image";
 import styles from "../styles/Login.module.css";
+import { Button, Modal } from 'antd';
 
 function Login() {
 
     let url="https://google.com";
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [firstPassword, setFirstPassword] = useState('');
+    const [firstLogin, setFirstLogin] = useState('');
+    const [firstLoginModal, setFirstLoginModal] = useState(false);
+    const [firstLoginModalUser, setFirstLoginModalUser] = useState(false);
+    const [forgetPasswordModal, setForgetPasswordModal] = useState(false);
+
+    let isAdmin = true;
 
     console.log("USERNAME =>", username);
     console.log("PASSWORD =>", password);
 
     const handleFirstLogin = () => {
         console.log("Click Première connexion");
+        if (isAdmin) {
+            setFirstLoginModal(true);
+            console.log("MODAL ?", firstLoginModal);
+            return
+        }
+        setFirstLoginModalUser(true);
     };
+
+    const handleCancelFirstLogin = () => {
+        setFirstLoginModal(false);
+        setFirstLoginModalUser(false);
+      };
+
+     const handleCancelForgetPassword = () => {
+        setForgetPasswordModal(false);
+     }
+
 
     const handleForgotPassword = () => {
         console.log("Click mdp oublié");
+        setForgetPasswordModal(true);
     };
 
     const handleSubmit = () => {
         console.log("Click on submit");
-    }
+    };
+
+    const handleSignUpSubmit = () => {
+        console.log("Sign Up Submit");
+    };
 
   return (
     <>
@@ -36,12 +64,12 @@ function Login() {
         <div className={styles.loginBox}>
             <h2 className={styles.title}>Connectez vous à votre espace</h2>
             <span className={styles.desc}>Identifiant / e-mail :</span>
-            <input type="text" className={styles.input} onChange={(e) => setUsername(e.target.value)} value={username} placeholder="Username" />
+            <input type="text" className={styles.input} onChange={(e) => setUsername(e.target.value)} value={username} placeholder="E-mail" />
             <span className={styles.links + ' ' + styles.first} onClick={() => handleFirstLogin()}>Première connexion ?</span>
             <span className={styles.desc + ' ' + styles.mdp}>Mot de passe :</span>
-            <input type="password" className={styles.input} onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Password" />
+            <input type="password" className={styles.input} onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Mot de passe" />
             <span className={styles.links} onClick={() => handleForgotPassword()}>Mot de passe oublié ?</span>
-            <button className={styles.button} onClick={() => handleSubmit()}>Connection</button>
+            <button className={styles.button} onClick={() => handleSubmit()}>Connexion</button>
         </div>
         <a href={url} target="_blank" className={styles.textContent}>Conditions d'utilisation</a>
         </div>
@@ -50,6 +78,25 @@ function Login() {
           src="/droiteaccueil.png"
           alt="Logo droite"
         />
+
+        <Modal onCancel={() => handleCancelFirstLogin()} open={firstLoginModal} footer={null}>
+            <div className={styles.loginBoxModal}>
+            <h2 className={styles.title}>Bienvenue chez EasyLease</h2>
+            <span className={styles.desc + ' ' + styles.descModal}>Nouvel identifiant / e-mail :</span>
+            <input type="text" className={styles.input + ' ' + styles.inputModal} onChange={(e) => setFirstLogin(e.target.value)} value={firstLogin} placeholder="E-mail" />
+            <span className={styles.desc + ' ' + styles.mdpModal + ' ' + styles.descModal}>Nouveau mot de passe :</span>
+            <input type="password" className={styles.input} onChange={(e) => setFirstPassword(e.target.value)} value={firstPassword} placeholder="Mot de passe" />
+            <button className={styles.button + ' ' + styles.modalBtn} onClick={() => handleSignUpSubmit()}>Création de compte</button>
+            </div>
+        </Modal>
+
+        <Modal onCancel={() => handleCancelFirstLogin()} open={firstLoginModalUser} footer={null}>
+            <p style={{fontSize: 18, textAlign: 'center'}}>Vous n'êtes pas l'administrateur vous ne pouvez pas vous créer de compte. Veuillez contacter l'administrateur !</p>
+        </Modal>
+
+        <Modal onCancel={() => handleCancelForgetPassword()} open={forgetPasswordModal} footer={null}>
+            <p style={{fontSize: 18, textAlign: 'center'}}>Merci de contacter l'administrateur en charge de votre compte !</p>
+        </Modal>
       </div>
     </>
   );
