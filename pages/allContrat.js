d03fe7afdd8f370f08aef0151399be5b38c75023
Nicolas <nicolas99@hotmail.fr>
2022-12-12 17:45:38 +0100

    partie contrat par  Nico

---
+++ pages/allContrat.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import AllContrat from "../components/AllContrat";
+
+function AllContratPage() {
+  return (
+    <>
+      <Head>
+        <title>AllContrat / EasyLease</title>
+      </Head>
+      <AllContrat />
+    </>
+  );
+}
+
+export default AllContratPage;
