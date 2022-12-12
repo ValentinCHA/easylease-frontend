0e7ba8498d119c14854115c62a6f8d9a257c8416
ezeflt <108800520+ezeflt@users.noreply.github.com>
2022-12-12 10:36:14 +0100

    nav bar mise à jour

--- components/Contrat.js
+++ components/Contrat.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from '../styles/Contacts.module.css'
+import style from '../styles/Contrat.module.css'
 import Navbar from './Navbar';
 
 function Contrat() {
