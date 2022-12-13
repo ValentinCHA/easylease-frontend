c0214898b0742300426db63bb1b126058bf4e75a
SBTH29 <115708643+SBTH29@users.noreply.github.com>
2022-12-13 17:05:27 +0100

    p5front

--- components/AllContrat.js
+++ components/AllContrat.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from '../styles/AllContratPage.module.css'
 import Navbar from './Navbar';
 import ContratCard from './ContratCard'
-import { faMountainCity } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 
 function AllContrat() {
+
+  const [inputValue, setInputValue] = useState('')
+  const [dataScenary, setDataScenary] = useState([])
+
 // tableau d'essaie
-  const array = [
-    {name:'Macbook air Apple', type:'Ordinateurs', durée:'24 Mois',date_de_début:'10/09/22' ,date_de_fin:'10/09/24' ,montant:'100.000€', valeur_résiduel: '10%'},
+  let array = [
+    {name:'Macbook air Apple', type:'Ordinateurs', durée:'24 Mois',date_de_début:'10/09/22' ,date_de_fin:'10/09/24' ,date_de_fin:'100.000€', valeur_résiduel: '10%'},
     {name:'Bureau Ikea', type:'Meubles', durée:'34 Mois',date_de_début:'10/09/23' ,date_de_fin:'10/09/26' ,montant:'200.000€', valeur_résiduel: '20%'},
     {name:'Mercedes cla200', type:'Voitures', durée:'12 Mois',date_de_début:'10/09/22' ,date_de_fin:'10/09/23' ,montant:'400.000€', valeur_résiduel: '20%'},
     {name:'Bmw x6', type:'voitures', durée:'12 Mois',date_de_début:'10/09/22' ,date_de_fin:'10/09/23' ,montant:'450.000€', valeur_résiduel: '10%'},
@@ -17,10 +24,41 @@ function AllContrat() {
     {name:'Chaise ', type:'Meubles', durée:'24 Mois',date_de_début:'10/09/22' ,date_de_fin:'10/09/24' ,montant:'100.000€', valeur_résiduel: '10%'},
   ]
 // map tableau d'essaie
-  const infoContrat = array.map((data,i)=>{
-    return <ContratCard {...data}/>
+  const infoContrat = dataScenary.filter((data)=>{
+    if(inputValue == ""){
+      return data
+    }else if(data.name.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase())){
+      return data
+    }
+  }).map((data,i)=>{
+    return <ContratCard {...data} />
   })
 
+  useEffect(()=>{
+    fetch('http://localhost:3000/scenary/all')
+    .then(response=>response.json())
+    .then(data=>{
+      if(data){
+        console.log('data find',data)
+        const scenary = data.scenaries.map((data,i)=>{
+          return ({
+            name: data.name,
+            type: data.type,
+            durée: data.duration,
+            montant: data.amount,
+            creationDate: data.creationDate,
+            date_de_début: data.contratStart,
+            date_de_fin: data.contratEnd,
+            valeur_résiduel: data.residualValue
+          })
+        })
+        setDataScenary(scenary)
+      }
+    })
+  },[])
+
+
+  console.log(inputValue)
   return (
     <>
     {/* navbar et header */}
@@ -30,15 +68,13 @@ function AllContrat() {
         <h1 className={style.head} >Contrat</h1>
       </div>
       <div className={style.container}>
-        {/* mes 2 inputs de recherche */}
+        {/* mon input de recherche */}
           <div className={style.search}>
-            <select className={style.select}>
-              <option className={style.select}>Select contrat.....</option>
-            </select>
-            <input className={style.input} type='text' placeholder='🔎 Search contrat.....'/>
+            <input className={style.input} onChange={(e)=>setInputValue(e.target.value)} type='text' placeholder='  📄 Search contrat.....' value={inputValue}/>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className={style.icon}/>
           </div>
           {/* span qui affiche le nom du client */}
-          <span style={{paddingLeft:120,borderBottom:'2px solid rgb(235,239,242)', width:'150%'}}>Contrats de l’entreprise “Nom du client” :</span>
+          <span style={{paddingLeft:120,borderBottom:'2px solid rgb(235,239,242)'}}>Contrats de l’entreprise “Nom du client” :</span>
           {/* div qui contiendra tout mes coponents contrat Card */}
           <div className={style.containerContratCard}>
             {infoContrat}
