b91e81de11ed33674a35608e23cbe712639ddf81
Nicolas <nicolas99@hotmail.fr>
2022-12-13 17:33:47 +0100

    client profil

---
+++ pages/contrat 2.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Contrat from '../components/Contrat';
+
+function ContratPage() {
+  return (
+    <>
+      <Head>
+        <title>Contrat / EasyLease</title>
+      </Head>
+      <Contrat />
+    </>
+  );
+}
+
+export default ContratPage;
