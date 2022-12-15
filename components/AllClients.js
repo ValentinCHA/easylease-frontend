1850117d6268f8ed2b180a22c35b4f7daf292303
SBTH29 <115708643+SBTH29@users.noreply.github.com>
2022-12-15 18:48:33 +0100

    ac

--- components/AllClients.js
+++ components/AllClients.js
@@ -1,13 +1,16 @@
-import Navbar from './Navbar';
-import style from '../styles/AllClients.module.css'
-import ClientCard from './ClientCard'
-import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect, useState } from "react";
+import style from "../styles/AllClients.module.css";
+import Navbar from "./Navbar";
+import ClientCard from "./ClientCard";
+import { addId } from "../reducers/contrat";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useSelector } from "react-redux";
 
-
-function AllClients() {
-
+function AllContrat() {
+  const [inputValue, setInputValue] = useState("");
   const [dataClient, setDataClient] = useState([]);
+
   const user = useSelector((state) => state.user.value);
 
   useEffect(() => {
@@ -15,33 +18,62 @@ function AllClients() {
       .then((response) => response.json())
       .then((data) => {
         if (data.result) {
+          // console.log('data find',data.contrat)
+          const client = data.clientsInfos.map((data, i) => {
+            return {
+              _id: data._id,
+              name: data.name,
+              address: data.address,
+              numberOfEmployees: data.numberOfEmployees,
+              clientBirth: data.clientBirth,
+              chiffre: data.chiffre,
 
-          setDataClient(data.clientsInfos);
+            };
+          });
+          setDataClient(client);
         }
       });
   }, []);
 
-  function clientsInfos(client) {
-    console.log(client);
-  }
+  const infoClient = dataClient.filter((data) => {
+    if (inputValue == "") {
+      return data;
+    } else if (
+      data.name.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase())
+    ) {
+      return data;
+    }
+  })
+    .map((data, i) => {
+      return <ClientCard key={i} {...data} id={data._id} />;
+    });
 
   return (
-
     <>
-      <Navbar />
-      <div className={style.header}>
-        <h1 className={style.head} >All Clients</h1>
-      </div>
-      <div className={style.container}>{dataClient.map((client, i) => {
-
-        return (
-          <button className={style.buttonTest}
-            onClick={() => clientsInfos(client)}>client {i}</button>
-        )
-      })}
+      {/* {/* navbar et header /} */}
+      <div className={style.main}>
+        <Navbar />
+        <div className={style.header}>
+          <h1 className={style.head}>AllClients</h1>
+        </div>
+        <div className={style.container}>
+          {/* {/ mon input de recherche /} */}
+          <div className={style.search}>
+            <input className={style.input} onChange={(e) => setInputValue(e.target.value)} type="text" placeholder="  Search contrat....." value={inputValue} />
+            <FontAwesomeIcon icon={faMagnifyingGlass} className={style.icon} />
+          </div>
+          {/* {/ span qui affiche le nom du client /} */}
+          <span
+            style={{ paddingLeft: 120, borderBottom: "2px solid rgb(235,239,242)" }}>Contrats de l’entreprise “Nom du client” :</span>
+          {/* {/ div qui contiendra tout mes coponents contrat Card /} */}
+          <div className={style.containerClientCard}>{infoClient}</div>
+        </div>
+        {/* span voir plus */}
+        <span
+          style={{ display: "flex", justifyContent: "center", alignItems: "center", color: "rgb(101,94,255)", cursor: "pointer", marginTop: -40, }}>voir plus...</span>
       </div>
     </>
-  )
+  );
 }
 
-export default AllClients;
+export default AllContrat;
--- components/ClientProfil.js
+++ components/ClientProfil.js
@@ -4,15 +4,14 @@ import style from '../styles/ClientProfil.module.css'
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import allClients from '../reducers/allClients';
+import client from '../reducers/client';
+import { useRouter } from 'next/router'
 // import { fa-sharp fa-solid fa-download } from '@fortawesome/free-solid-svg-icons';
 
 function ClientProfil() {
+    const router = useRouter()
+    console.log('log du query', router.query);
 
-    const user = useSelector((state) => state.user.value);
-    const allClients = useSelector((state) => state.allClients.value);
-
-	
     return (
         <>
             <div className={style.mainContainer}>
@@ -20,25 +19,20 @@ function ClientProfil() {
                 <div className={style.header}>
                     <h1 className={style.head} >Profil client</h1>
                 </div>
-
                 <div className={style.container}>
                     <div className={style.ButtonContainer}>
                         <button className={style.buttonmodifier} onClick={() => ModifClient()}>Modifier</button>
                         <button className={style.buttonsupprimer} onClick={() => SupprimClient()}>Supprimer</button>
                     </div>
                     <div className={style.GridContent}>
-
                         <div className={style.Infosclient}>
-                            {clients}
                             <h2>Informations client : </h2>
                             <ul>
-                                <li>Nom entreprise : </li>
-                                <li>Interlocuteur : </li><li>Téléphone : </li>
-                                <li>Client depuis le : </li>
-                                <li>Adresse : </li>
-                                <li>Email : </li>
-                                <li>Nombre de salariés : </li>
-                                <li>Chiffre d'affaires : </li>
+                                <li>Nom entreprise : {router.query.name}</li>
+                                <li>Client depuis le : {router.query.clientBirth}</li>
+                                <li>Adresse : {router.query.address}</li>
+                                <li>Nombre de salariés : {router.query.numberOfEmployees} </li>
+                                <li>Chiffre d'affaires : {router.query.chiffre} </li>
                             </ul>
                         </div>
                         <div className={style.RightContent}>
@@ -46,8 +40,6 @@ function ClientProfil() {
                             <button className={style.buttonAddDoc} onClick={() => addDocClient()}>Ajouter document</button>
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </>
