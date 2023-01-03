cb73b58b34e1ee40dd02a89a4fe80fb8d66a5e4c
Valentin Charpentier <valentincharpentier@hotmail.com>
2023-01-03 16:25:05 +0100

    5

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,14 @@
 import Head from 'next/head';
 import Dashboard from '../components/Dashboard';
 // import { redirect } from "../components/isLogged.js"
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 
 function DashboardPage() {
 
+  const user = useSelector((state) => state.user.value);
+  
+const router = useRouter();
   // redirect("dashboard");
   function redirect(page) {
 
