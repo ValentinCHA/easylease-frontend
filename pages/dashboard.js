b5e4f934366a7154491e4bf9dd69d3784a462046
Valentin Charpentier <valentincharpentier@hotmail.com>
2023-01-03 16:27:49 +0100

    with import redirect function

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,24 +1,24 @@
 import Head from 'next/head';
 import Dashboard from '../components/Dashboard';
-// import { redirect } from "../components/isLogged.js"
+import { redirect } from "../components/isLogged.js"
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
 function DashboardPage() {
 
-  const user = useSelector((state) => state.user.value);
+//   const user = useSelector((state) => state.user.value);
   
-const router = useRouter();
-  // redirect("dashboard");
-  function redirect(page) {
+// const router = useRouter();
+  redirect("dashboard");
+//   function redirect(page) {
 
-    if (!user.token) {
-      return router.push('/login');
-    } else {
-      return router.push(`/${page}`)
-    };
-};
-redirect("dashboard")
+//     if (!user.token) {
+//       return router.push('/login');
+//     } else {
+//       return router.push(`/${page}`)
+//     };
+// };
+// redirect("dashboard")
   
   return (
     <>
